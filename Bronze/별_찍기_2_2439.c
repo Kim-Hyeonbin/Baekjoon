bb205b8ae446747c57a7d08178a73bbbe1c78a9f
Hyeonbin <bin031001@gmial.com>
2025-05-20 20:56:44 +0900

    별찍기 1일차

---
+++ Bronze/별_찍기_2_2439.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = N; j > i + 1; j--) {
+			printf(" ");
+		}
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
