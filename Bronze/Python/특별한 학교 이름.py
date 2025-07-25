c5aa1b5e0a79fbf7024379a2f6a0fedc9fcb7124
김현빈 <bin031001@gmail.com>
2025-07-25 22:57:35 +0900

    Solve 27889 : 특별한 학교 이름

---
+++ Bronze/Python/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+schoolName = input()
+
+if schoolName == "NLCS":
+    print("North London Collegiate School")
+elif schoolName == "BHA":
+    print("Branksome Hall Asia")
+elif schoolName == "KIS":
+    print("Korea International School")
+elif schoolName == "SJA":
+    print("St. Johnsbury Academy")
