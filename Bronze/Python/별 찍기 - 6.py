1e5dd7932d75e9c35fa7432b6cc34851e84c7680
김현빈 <bin031001@gmail.com>
2025-07-26 19:06:46 +0900

    Solve 2443 : 별 찍기 - 6

---
+++ Bronze/Python/별 찍기 - 6.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(2 * N - (2 * i + 1)):
+        print("*", end="")
+    print()
