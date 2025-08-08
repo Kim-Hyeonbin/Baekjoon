2d26a45fd3239a8098fe000313ed356c7622cf4e
김현빈 <bin031001@gmail.com>
2025-08-08 17:16:40 +0900

    Solve 28074 : 모비스

---
+++ Bronze/Python/모비스.py
@@ -0,0 +1,10 @@
+input_text = input()
+print(
+    "YES"
+    if "M" in input_text
+    and "O" in input_text
+    and "B" in input_text
+    and "I" in input_text
+    and "S" in input_text
+    else "NO"
+)
