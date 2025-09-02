f57967302a76ed839fabe0c8fbe820a5ada7ffa9
김현빈 <bin031001@gmail.com>
2025-09-02 23:18:20 +0900

    Solve 1929 : 소수 구하기

---
+++ Silver/소수 구하기.py
@@ -0,0 +1,13 @@
+M, N = map(int, input().split())
+
+for i in range(M, N + 1):
+    isprime = True
+    if i == 1:
+        continue
+
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            isprime = False
+            break
+    if isprime:
+        print(i)
