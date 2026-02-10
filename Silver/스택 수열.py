7a871ce6bdb5c2f60c49cffb4a5c822f1baf56bb
김현빈 <bin031001@gmail.com>
2026-02-10 16:47:01 +0900

    Solve 1874 : 스택 수열

---
+++ Silver/스택 수열.py
@@ -0,0 +1,26 @@
+import sys
+
+next = 1
+stack = []
+
+n = int(sys.stdin.readline())
+result = ""
+
+for _ in range(n):
+    target = int(sys.stdin.readline())
+
+    while next <= target:
+        stack.append(next)
+        next += 1
+        result += "+"
+
+    if stack[-1] <= target:
+        del stack[-1]
+        result += "-"
+    elif stack[-1] > target:
+        result = "NO"
+        break
+    else:
+        continue
+
+print("NO" if result == "NO" else "\n".join(result))
